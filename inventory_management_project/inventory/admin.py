40747b073eca4dc5153dc9350f553c501a8e705a
Melek78 <abemelek78@gmail.com>
2025-10-05 18:16:04 +0300

    created inventoryitem model and registered it to admins.py

--- inventory_management_project/inventory/admin.py
+++ inventory_management_project/inventory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import InventoryItem
 
-# Register your models here.
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'quantity', 'category', 'price', 'last_updated')
+    search_fields = ('name', 'category', 'user__username')
--- inventory_management_project/inventory/models.py
+++ inventory_management_project/inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class InventoryItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'inventory_items')
+    name = models.CharField(max_length= 100)
+    description = models.TextField(blank= True)
+    quantity = models.PositiveIntegerField(default= 0)
+    price = models.DecimalField(max_digits= 10, decimal_places= 2)
+    category = models.CharField(max_length= 50, blank= True)
+    date_added = models.DateTimeField(auto_now_add= True)
+    last_updated = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity} pcs)"
